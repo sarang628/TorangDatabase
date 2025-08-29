55c0f32d6fc3239f71bc98e4a14acf4960b43251
sryang <sarang628@gmail.com>
2025-08-29 15:05:22 +0800

    separate TestLoggedInUserDao

---
+++ library/src/main/java/com/sarang/torang/core/database/dao/test/TestLoggedInUserDao.kt
@@ -0,0 +1,33 @@
+package com.sarang.torang.core.database.dao.test
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.material3.HorizontalDivider
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.sp
+import com.sarang.torang.core.database.dao.LoggedInUserDao
+
+@Composable
+fun TestLoggedInUserDao(loggedInUserDao: LoggedInUserDao) {
+    var isLogin by remember { mutableStateOf(false) }
+
+    LaunchedEffect(key1 = "", block = {
+        loggedInUserDao.getLoggedInUser1()?.let {
+            isLogin = true
+        }
+    })
+
+    Column {
+        HorizontalDivider(color = Color.LightGray)
+        Text(text = "TestLoggedInUserDao", fontWeight = FontWeight.Bold, fontSize = 20.sp)
+        Text(text = "${isLogin}")
+        HorizontalDivider(color = Color.LightGray)
+    }
+}
