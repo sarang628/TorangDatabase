1363a0dabddc11a06814ab4d8ee4fb74577a5ffd
sryang <sarang628@gmail.com>
2025-08-29 15:03:51 +0800

    saparate LoggedInUserDaoTest

--- library/src/main/java/com/sarang/torang/core/database/dao/LoggedInUserDao.kt
+++ library/src/main/java/com/sarang/torang/core/database/dao/LoggedInUserDao.kt
@@ -1,17 +1,5 @@
 package com.sarang.torang.core.database.dao
 
-import androidx.compose.foundation.layout.Column
-import androidx.compose.material3.HorizontalDivider
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.LaunchedEffect
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.unit.sp
 import androidx.room.Dao
 import androidx.room.Insert
 import androidx.room.OnConflictStrategy
@@ -41,22 +29,4 @@ interface LoggedInUserDao {
 
     @Query("select COUNT(*) from LoggedInUserEntity")
     fun isLogin(): Flow<Int>
-}
-
-@Composable
-fun TestLoggedInUserDao(loggedInUserDao: LoggedInUserDao) {
-    var isLogin by remember { mutableStateOf(false) }
-
-    LaunchedEffect(key1 = "", block = {
-        loggedInUserDao.getLoggedInUser1()?.let {
-            isLogin = true
-        }
-    })
-
-    Column {
-        HorizontalDivider(color = Color.LightGray)
-        Text(text = "TestLoggedInUserDao", fontWeight = FontWeight.Bold, fontSize = 20.sp)
-        Text(text = "${isLogin}")
-        HorizontalDivider(color = Color.LightGray)
-    }
 }
