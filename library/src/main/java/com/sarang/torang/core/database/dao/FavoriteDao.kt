d64458c1109d0ad3e1d4e840467f1bf639c6486f
sryang <sarang628@gmail.com>
2025-08-29 15:03:15 +0800

    saparate FavoriteDaoTest

--- library/src/main/java/com/sarang/torang/core/database/dao/FavoriteDao.kt
+++ library/src/main/java/com/sarang/torang/core/database/dao/FavoriteDao.kt
@@ -1,14 +1,5 @@
 package com.sarang.torang.core.database.dao
 
-import androidx.compose.foundation.layout.Column
-import androidx.compose.material3.Button
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.rememberCoroutineScope
-import androidx.compose.runtime.setValue
 import androidx.room.Dao
 import androidx.room.Delete
 import androidx.room.Insert
@@ -17,14 +8,12 @@ import androidx.room.Query
 import com.sarang.torang.core.database.model.favorite.FavoriteEntity
 import com.sarang.torang.core.database.model.feed.ReviewAndImageEntity
 import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.launch
 
 @Dao
 interface FavoriteDao {
 
     @Query("select count(*) from FavoriteEntity where reviewId = (:reviewId)")
     suspend fun hasFavorite(reviewId: Int): Int
-
 
     @Delete
     suspend fun deleteFavorite(favorite: FavoriteEntity)
@@ -38,20 +27,17 @@ interface FavoriteDao {
     @Query("select * from FavoriteEntity where reviewId = (:reviewId)")
     fun getFavorite(reviewId: Int): Flow<FavoriteEntity>
 
-    @Query(
-        """
+    @Query("""
         SELECT FeedEntity.*, UserEntity.profilePicUrl, UserEntity.userId
         FROM FeedEntity 
         JOIN UserEntity ON FeedEntity.userId =  UserEntity.userId
         LEFT OUTER JOIN RestaurantEntity ON FeedEntity.restaurantId = RestaurantEntity.restaurantId
         WHERE reviewId IN (Select reviewId from FavoriteEntity where userId = (:userId) )
         ORDER BY createDate DESC
-        """
-    )
+        """)
     fun getMyFavorite(userId: Int): Flow<List<ReviewAndImageEntity>>
 
-    @Query(
-        """
+    @Query("""
         DELETE FROM FavoriteEntity
     """
     )
@@ -59,25 +45,4 @@ interface FavoriteDao {
 
     @Insert(onConflict = OnConflictStrategy.REPLACE)
     fun insertAll(feedList: List<FavoriteEntity>)
-}
-
-@Composable
-fun FavoriteDaoTest(feedDao: FavoriteDao) {
-    var text by remember { mutableStateOf("") }
-    val coroutineScope = rememberCoroutineScope()
-    Column {
-        Button(onClick = {
-            coroutineScope.launch {
-                feedDao.getMyFavorite(1).collect {
-                    text = "" + it
-                }
-            }
-        }) {
-
-        }
-        Button(onClick = { feedDao.getFavorite(1) }) {
-
-        }
-        Text(text = text)
-    }
 }
