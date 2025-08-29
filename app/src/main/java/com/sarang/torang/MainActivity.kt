ef5f51b9a9f2637b01143a9a9ff89b70cf251a56
sryang <sarang628@gmail.com>
2025-08-29 15:05:44 +0800

    set hilt

--- app/src/main/java/com/sarang/torang/MainActivity.kt
+++ app/src/main/java/com/sarang/torang/MainActivity.kt
@@ -13,7 +13,9 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.sarang.torang.core.database.AppDatabase
 import com.sarang.torang.ui.theme.TorangDatabaseTheme
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
