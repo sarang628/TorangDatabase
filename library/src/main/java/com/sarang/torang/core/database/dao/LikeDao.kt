c1dc327761b88c7cd2f90a5f8bcf262e4a893770
sryang <sarang628@gmail.com>
2025-11-28 10:43:16 +0900

    arrange code

--- library/src/main/java/com/sarang/torang/core/database/dao/LikeDao.kt
+++ library/src/main/java/com/sarang/torang/core/database/dao/LikeDao.kt
@@ -11,21 +11,22 @@ import kotlinx.coroutines.flow.Flow
 @Dao
 interface LikeDao {
     @Query("""SELECT * 
-        from LikeEntity 
-        where reviewId = (:reviewId)""")                   fun getByReviewIdFlow(reviewId: Int)        : Flow<LikeEntity>
+                      FROM LikeEntity 
+                      WHERE reviewId = (:reviewId)""")         fun getByReviewIdFlow(reviewId: Int)        : Flow<LikeEntity>
     @Query("""SELECT * 
                       FROM LikeEntity 
-                      WHERE reviewId = (:reviewId)""")           suspend fun getByReviewId(reviewId: Int)            : LikeEntity?
-    @Insert                              suspend fun add(like: LikeEntity)
+                      WHERE reviewId = (:reviewId)""") suspend fun getByReviewId(reviewId: Int)            : LikeEntity?
+    @Insert                    suspend fun add(like: LikeEntity)
     @Insert(onConflict = OnConflictStrategy.REPLACE)
-                                         suspend fun addAll(likes: List<LikeEntity>)
+                               suspend fun addAll(likes: List<LikeEntity>)
     @Query("""DELETE
-        FROM LikeEntity
-        WHERE likeId = :likeId""")             suspend fun delete(likeId: Int)
+                      FROM LikeEntity
+                      WHERE likeId = :likeId""")   suspend fun delete(likeId: Int)
     @Query("""DELETE 
-            FROM LikeEntity""")             suspend fun deleteAll()
-    @Delete                              suspend fun deleteAll(likes: List<LikeEntity>)
-    @Query("""SELECT count(*) 
-                    FROM LikeEntity 
-                    WHERE reviewId = (:reviewId)""")    suspend fun has(reviewId: Int)                      : Int
+                      FROM LikeEntity""")   suspend fun deleteAll()
+    @Delete                    suspend fun deleteAll(likes: List<LikeEntity>)
+    @Query("""SELECT 
+                             count(*) 
+                      FROM LikeEntity 
+                      WHERE reviewId = (:reviewId)""")   suspend fun has(reviewId: Int)                      : Int
 }
