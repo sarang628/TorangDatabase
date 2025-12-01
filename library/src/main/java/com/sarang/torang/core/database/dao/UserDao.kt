6695a468e2ef6bd8aa00ba75b0956f49fcb89c87
sryang <sarang628@gmail.com>
2025-12-01 15:38:32 +0900

    refactor UserDao

--- library/src/main/java/com/sarang/torang/core/database/dao/UserDao.kt
+++ library/src/main/java/com/sarang/torang/core/database/dao/UserDao.kt
@@ -11,66 +11,53 @@ import kotlinx.coroutines.flow.Flow
 
 @Dao
 interface UserDao {
-    @Query("SELECT * FROM UserEntity")
-    fun findAllFlow(): Flow<List<UserEntity>>
-
-    @Query("SELECT * FROM UserEntity")
-    suspend fun findAll(): List<UserEntity>
-
-    @Query("SELECT * FROM UserEntity WHERE userId IN (:userIds)")
-    fun findAllById(userIds: IntArray): List<UserEntity>
-
+    @Query("""
+        SELECT *
+        FROM UserEntity
+        """)                     fun findAllFlow()                  : Flow<List<UserEntity>>
+    @Query("""SELECT *
+                      FROM UserEntity""")             suspend fun findAll()                      : List<UserEntity>
+    @Query("""SELECT * 
+                      FROM UserEntity 
+                      WHERE userId IN (:userIds)""")                     fun findAllById(userIds: IntArray) : List<UserEntity>
+    @Query("""SELECT * 
+                      FROM UserEntity 
+                      WHERE userId = (:userId)""")                     fun findByIdFlow(userId: Int)      : Flow<UserEntity>
+    @Query("""SELECT * 
+                      FROM UserEntity 
+                      WHERE userId = (:userId)""")             suspend fun findById(userId: Int)          : UserEntity?
+    @Query("""UPDATE UserEntity 
+                      SET userName = :userName ,profilePicUrl = :profilePicUrl  
+                      WHERE userId = :userId""")    suspend fun update(userId: Int, userName: String, profilePicUrl: String)
+    @Query("""SELECT COUNT(*)
+                      FROM UserEntity 
+                      WHERE userId = :userId""")      suspend fun exists(userId: Int): Int
+    @Query("""UPDATE UserEntity 
+                      SET userName = :userName ,profilePicUrl = :profilePicUrl  
+                      WHERE userId = :userId""")    suspend fun updateByChatRoom(userId: Int, userName: String, profilePicUrl: String)
+    @Delete                                suspend fun delete(user: UserEntity)
+    @Insert(onConflict = OnConflictStrategy.REPLACE) @Transaction
+                                           suspend fun addAll(users: List<UserEntity>)
     @Insert(onConflict = OnConflictStrategy.REPLACE)
-    @Transaction
-    suspend fun addAll(users: List<UserEntity>)
-
-    @Insert(onConflict = OnConflictStrategy.REPLACE)
-    suspend fun addUser(user: UserEntity)
-
-    @Delete
-    suspend fun delete(user: UserEntity)
-
-    @Query("select * from UserEntity where userId = (:userId)")
-    fun findByIdFlow(userId: Int): Flow<UserEntity>
-
-    @Query("select * from UserEntity where userId = (:userId)")
-    suspend fun findById(userId: Int): UserEntity?
-
-    @Query("update UserEntity set userName = :userName ,profilePicUrl = :profilePicUrl  where userId = :userId")
-    suspend fun update(userId: Int, userName: String, profilePicUrl: String)
-
-    @Query("SELECT COUNT(*) FROM UserEntity WHERE userId = :userId")
-    suspend fun exists(userId: Int): Int
-
-    @Query("update UserEntity set userName = :userName ,profilePicUrl = :profilePicUrl  where userId = :userId")
-    suspend fun updateByChatRoom(userId: Int, userName: String, profilePicUrl: String)
-
-    @Transaction
-    suspend fun insertOrUpdateUser(users: List<UserEntity>) {
-        users.forEach { user ->
-            if (exists(user.userId) > 0) {
-                updateByChatRoom(
-                    user.userId,
-                    user.userName,
-                    user.profilePicUrl
-                )
-            } else {
-                addUser(
-                    UserEntity(
-                        userId = user.userId,
-                        userName = user.userName,
-                        email = "",
-                        loginPlatform = "",
-                        createDate = "",
-                        accessToken = "",
-                        profilePicUrl = user.profilePicUrl,
-                        point = 0,
-                        reviewCount = "",
-                        followers = "",
-                        following = ""
-                    )
-                )
-            }
-        }
+                                           suspend fun addUser(user: UserEntity)
+                                           suspend fun insertOrUpdateUser(user: UserEntity) {
+        if (exists(user.userId) > 0) {
+            updateByChatRoom(userId = user.userId,
+                             userName = user.userName,
+                             profilePicUrl = user.profilePicUrl) }
+        else {
+            addUser(UserEntity(userId = user.userId,
+                               userName = user.userName,
+                               email = user.email,
+                               loginPlatform = user.loginPlatform,
+                               createDate = user.createDate,
+                               accessToken = user.accessToken,
+                               profilePicUrl = user.profilePicUrl,
+                               point = 0,
+                               reviewCount = user.reviewCount,
+                               followers = user.following,
+                               following = user.following)) }
     }
+    @Transaction                           suspend fun insertOrUpdateUsers(users: List<UserEntity>) {
+        users.forEach { user -> insertOrUpdateUser(user = user) } }
 }
