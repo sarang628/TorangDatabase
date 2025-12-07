12fa515781126de497236aa91dbafcb22b948a86
sryang <sarang628@gmail.com>
2025-12-07 17:36:31 +0900

    set properties nullable in FeedEntity

--- library/src/main/java/com/sarang/torang/core/database/model/feed/FeedEntity.kt
+++ library/src/main/java/com/sarang/torang/core/database/model/feed/FeedEntity.kt
@@ -6,15 +6,15 @@ import androidx.room.PrimaryKey
 @Entity
 data class FeedEntity(
     @PrimaryKey
-    val reviewId        : Int = -1,
-    val userId          : Int,
-    val restaurantId    : Int?,
-    val userName        : String,
-    val restaurantName  : String?,
-    val profilePicUrl   : String,
-    val contents        : String,
-    val rating          : Float,
-    val likeAmount      : Int,
-    val commentAmount   : Int,
-    val createDate      : String
+    val reviewId        : Int       = -1,
+    val userId          : Int?      = null,
+    val restaurantId    : Int?      = null,
+    val userName        : String?   = null,
+    val restaurantName  : String?   = null,
+    val profilePicUrl   : String?   = null,
+    val contents        : String?   = null,
+    val rating          : Float?    = null,
+    val likeAmount      : Int?      = null,
+    val commentAmount   : Int?      = null,
+    val createDate      : String?   = null
 )
