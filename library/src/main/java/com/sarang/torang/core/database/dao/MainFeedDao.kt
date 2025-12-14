386b214eca4deda12f569d0e5fcba38478befe0e
sryang <sarang628@gmail.com>
2025-12-15 08:21:18 +0900

    set order to findAllFlow to main feed

--- library/src/main/java/com/sarang/torang/core/database/dao/MainFeedDao.kt
+++ library/src/main/java/com/sarang/torang/core/database/dao/MainFeedDao.kt
@@ -2,6 +2,7 @@ package com.sarang.torang.core.database.dao
 
 import androidx.room.Dao
 import androidx.room.Insert
+import androidx.room.OnConflictStrategy
 import androidx.room.Query
 import com.sarang.torang.core.database.model.feed.MainFeedEntity
 import com.sarang.torang.core.database.model.feed.ReviewAndImageEntity
@@ -9,19 +10,15 @@ import kotlinx.coroutines.flow.Flow
 
 @Dao
 interface MainFeedDao {
-    @Query("""
-        SELECT *
-        FROM FeedEntity
-        WHERE reviewId in (SELECT   reviewId 
-                           FROM     MainFeedEntity
-                           ORDER BY `order`)
-    """)          fun findAllFlow() : Flow<List<ReviewAndImageEntity>>
-    @Insert                     suspend fun addAll(list : List<MainFeedEntity>)
+    @Query(""" SELECT f.*
+                       FROM FeedEntity f
+                       JOIN MainFeedEntity m ON f.reviewId = m.reviewId
+                       ORDER BY m.`order` ASC """)        fun findAllFlow(): Flow<List<ReviewAndImageEntity>>
+    @Insert(onConflict = OnConflictStrategy.REPLACE )
+                                suspend fun addAll(list : List<MainFeedEntity>)
     @Insert                     suspend fun all(entity : MainFeedEntity)
-
     @Query("""DELETE
                       FROM MainFeedEntity""")    suspend fun deleteAll()
-
     @Query("""DELETE
                       FROM MainFeedEntity
                       WHERE reviewId = :reviewId""")    suspend fun deleteById(reviewId : Int)
